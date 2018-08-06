20228b726cca01cfe54c56930867d2631f924576
HiroshiOnodera <chuu.sora.863@gmail.com>
2018-08-06 11:35:25 +0900

    リファクタリング クラス名変更、メソッド名変更、メソッド抽出

--- test/test_account.py
+++ test/test_account.py
@@ -10,7 +10,7 @@ import unittest
 import os
 import yaml
 from passlib.hash import pbkdf2_sha256
-from user_information_in_yaml.account.account_editor import AccountEditor
+from user_information_in_yaml.account.account_editor import Account
 from user_information_in_yaml.account.item import Item
 
 
@@ -31,7 +31,7 @@ class TestAccountPakage(unittest.TestCase):
     @classmethod
     def setUp(cls):
         # class to be tested
-        cls.account = AccountEditor(cls.account_file_path)
+        cls.account = Account(cls.account_file_path)
         pass
 
     @classmethod
@@ -59,8 +59,8 @@ class TestAccountPakage(unittest.TestCase):
         error_account_email = 'error@email.com'
         error_account_password = 'error_password'
 
-        self.account.add_account(new_account_email_1, new_account_password_1)
-        self.account.add_account(new_account_email_2, new_account_password_2)
+        self.account.add(new_account_email_1, new_account_password_1)
+        self.account.add(new_account_email_2, new_account_password_2)
 
         self.assertTrue(self.account.authenticate(
             new_account_email_1, new_account_password_1))
@@ -78,11 +78,10 @@ class TestAccountPakage(unittest.TestCase):
         '''
         new_account_email_1 = 'your1@email.com'
         new_account_password_1 = 'your1_password'
-        self.account.add_account(new_account_email_1, new_account_password_1)
+        self.account.add(new_account_email_1, new_account_password_1)
 
         with self.assertRaises(KeyError):
-            self.account.add_account(
-                new_account_email_1, new_account_password_1)
+            self.account.add(new_account_email_1, new_account_password_1)
 
     def test_authenticate_with_no_account_file(self):
         email = 'your@email.com'
@@ -115,10 +114,9 @@ class TestAccountPakage(unittest.TestCase):
         account_email_error = 'error@email.com'
         account_password_error = 'error_password'
 
-        self.account.add_account(
-            account_email_deleted, account_password_deleted)
-        self.account.add_account(account_email, account_password)
-        self.account.delete_account(account_email_deleted)
+        self.account.add(account_email_deleted, account_password_deleted)
+        self.account.add(account_email, account_password)
+        self.account.delete(account_email_deleted)
 
         self.assertTrue(self.account.authenticate(
             account_email, account_password))
@@ -126,7 +124,7 @@ class TestAccountPakage(unittest.TestCase):
             account_email_deleted, account_password_deleted))
 
         with self.assertRaises(KeyError):
-            self.account.delete_account(account_email_error)
+            self.account.delete(account_email_error)
 
         self.assertFalse(self.account.authenticate(
             account_email_error, account_password_error))
@@ -138,13 +136,14 @@ class TestAccountPakage(unittest.TestCase):
         '''
         account_email_deleted = 'your1@email.com'
         account_password_deleted = 'your1_password'
-        self.account.add_account(
+        self.account.add(
             account_email_deleted, account_password_deleted)
 
         with self.assertRaises(RuntimeError):
-            self.account.delete_account(account_email_deleted)
+            self.account.delete(account_email_deleted)
 
-        self.assertTrue(self.account.authenticate(account_email_deleted, account_password_deleted))
+        self.assertTrue(self.account.authenticate(
+            account_email_deleted, account_password_deleted))
 
 
 if __name__ == '__main__':
--- user_information_in_yaml/account/account_editor.py
+++ user_information_in_yaml/account/account_editor.py
@@ -1,62 +1,62 @@
 '''
-account editor
+account contrller
 '''
 import yaml
 from passlib.hash import pbkdf2_sha256
 from user_information_in_yaml.account.item import Item
 
 
-class AccountEditor():
+class Account():
 
     def __init__(self, account_file_path):
         self.account_file_path = account_file_path
 
-    def add_account(self, email: str, password: str) -> None:
+    def __write_accoutn_file(self, account: dict) -> None:
+        with open(self.account_file_path, 'w') as account_file:
+            yaml.dump(account, stream=account_file, default_flow_style=False)
+
+    def __load_account_file(self) -> dict:
+        with open(self.account_file_path, 'r') as account_file:
+            return yaml.load(account_file)
+
+    def add(self, email: str, password: str) -> None:
         ''' add new account that email and password
         Raise KeyError
         '''
         hash_password = pbkdf2_sha256.hash(password)
 
         try:
-            with open(self.account_file_path, 'r') as account_file:
-                account = yaml.load(account_file)
-                if(email in account):
-                    raise KeyError
-                account[email] = {Item.PASSWORD.value: hash_password}
-
+            account = self.__load_account_file()
+            if(email in account):
+                raise KeyError
+            account[email] = {Item.PASSWORD.value: hash_password}
         except IOError:
-            with open(self.account_file_path, 'w') as account_file:
+            with open(self.account_file_path, 'w'):
                 account = {email: {Item.PASSWORD.value: hash_password}}
 
-        with open(self.account_file_path, 'w') as account_file:
-            yaml.dump(account, stream=account_file, default_flow_style=False)
+        self.__write_accoutn_file(account)
 
     def authenticate(self, email: str, password: str) -> bool:
         ''' authenticate using email and password
         '''
         try:
-            with open(self.account_file_path, 'r') as account_file:
-                account = yaml.load(account_file)
+            account = self.__load_account_file()
         except IOError:
             return False
 
         try:
             return pbkdf2_sha256.verify(password, account[email][Item.PASSWORD.value])
-
         except KeyError:
             return False
 
-    def delete_account(self, email: str) -> None:
+    def delete(self, email: str) -> None:
         ''' delete account using email
         Raises KeyError when The account file not have the email
         Raises RuntimeError when The account file have one account
         '''
-        with open(self.account_file_path, 'r') as account_file:
-            account = yaml.load(account_file)
-
+        account = self.__load_account_file()
         account.pop(email)
         if(not account):
             raise RuntimeError
 
-        with open(self.account_file_path, 'w') as account_file:
-            yaml.dump(account, stream=account_file, default_flow_style=False)
+        self.__write_accoutn_file(account)
